95cc943b9ad37e0f7badb1ae7f2f7fb0669df31e
AllPoland <52187288+AllPoland@users.noreply.github.com>
2025-12-29 21:04:56 -0700

    archipelago client connection

---
+++ AP/Client.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Threading.Tasks;
+using Archipelago.MultiClient;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Packets;
+using UNBEATAP;
+
+namespace unbeatAP.AP;
+
+public class Client
+{
+    public bool Connected = false;
+
+    public ArchipelagoSession Session;
+
+    private string ip;
+    private int port;
+    private string slot;
+    private string password;
+    private bool deathLink;
+
+
+    public Client(string ip, int port, string slot, string password, bool deathLink)
+    {
+        this.ip = ip;
+        this.port = port;
+        this.slot = slot;
+        this.password = password;
+        this.deathLink = deathLink;
+
+        Plugin.Logger.LogInfo($"Creating session with server {ip}:{port}");
+        Session = ArchipelagoSessionFactory.CreateSession(ip, port);
+    }
+
+
+    public async Task Connect()
+    {
+        Plugin.Logger.LogInfo($"Connecting to {ip}:{port} with game {Plugin.GameName} as {slot}");
+        LoginResult result;
+        try
+        {
+            await Session.ConnectAsync();
+            result = await Session.LoginAsync(
+                Plugin.GameName,
+                slot,
+                ItemsHandlingFlags.AllItems,
+                null,
+                deathLink ? ["DeathLink"] : null,
+                null,
+                string.IsNullOrEmpty(password) ? null : password,
+                true
+            );
+        }
+        catch(Exception e)
+        {
+            result = new LoginFailure(e.GetBaseException().Message);
+        }
+
+        if(!result.Successful)
+        {
+            LoginFailure failure = (LoginFailure)result;
+            string message = $"Failed to connect to {ip} as {slot}:";
+            foreach(string error in failure.Errors)
+            {
+                message += $"\n    {error}";
+            }
+            foreach(ConnectionRefusedError error in failure.ErrorCodes)
+            {
+                message += $"\n    {error}";
+            }
+
+            Plugin.Logger.LogError(message);
+            Connected = false;
+        }
+
+        Connected = true;
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,9 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using Arcade.Progression;
 using UNBEATAP.Patches;
-using UnityEngine.SceneManagement;
+using unbeatAP.AP;
+using BepInEx.Configuration;
 
 namespace UNBEATAP;
 
@@ -11,36 +11,74 @@ namespace UNBEATAP;
 [BepInProcess("UNBEATABLE.exe")]
 public class Plugin : BaseUnityPlugin
 {
+    public const string GameName = "UNBEATABLE Arcade";
+
     internal static new ManualLogSource Logger;
 
+    public static Client Client;
 
-    private void Awake()
+    private ConfigEntry<string> configIp;
+    private ConfigEntry<int> configPort;
+    private ConfigEntry<string> configSlot;
+    private ConfigEntry<string> configPassword;
+
+    private ConfigEntry<bool> configDeathLink;
+
+
+    private void LoadConfig()
+    {
+        configIp = Config.Bind(
+            "Connection",
+            "IP",
+            "archipelago.gg",
+            "The archipelago server IP to connect to. If you're running the game on the official website, this is probably 'archipelago.gg'."
+        );
+        configPort = Config.Bind(
+            "Connection",
+            "Port",
+            58008,
+            "The port to connect to. This is all the numbers after the ':' in the IP."
+        );
+        configSlot = Config.Bind(
+            "Connection",
+            "Slot",
+            "Player1",
+            "The player slot name to connect with. Enter what you set 'name' to in your YAML."
+        );
+        configPassword = Config.Bind(
+            "Connection",
+            "Password",
+            "",
+            "The password to use when connecting. If the server has no password, leave this empty."
+        );
+
+        configDeathLink = Config.Bind(
+            "Gameplay",
+            "DeathLink",
+            false,
+            "If Death Link is enabled, you'll fail out of a song if someone else dies, and everyone else will die when you fail a song."
+        );
+    }
+
+
+    private async void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
+
+        LoadConfig();
+
+        Client = new Client(configIp.Value, configPort.Value, configSlot.Value, configPassword.Value, configDeathLink.Value);
+        await Client.Connect();
+
+        Logger.LogInfo("Patching");
         // ArcadeSongView seems pretty useless if ArcadeDifficultyView is enabled, maybe it can be unpatched on archipelago connection?
-        //Harmony.CreateAndPatchAll(typeof(ArcadeSongView));
-        //Harmony.CreateAndPatchAll(typeof(ArcadeCharacterView));
-        //Harmony.CreateAndPatchAll(typeof(ArcadeDifficultyView));
+        // Harmony.CreateAndPatchAll(typeof(ArcadeSongView));
+        // Harmony.CreateAndPatchAll(typeof(ArcadeCharacterView));
+        // Harmony.CreateAndPatchAll(typeof(ArcadeDifficultyView));
         Harmony.CreateAndPatchAll(typeof(BlockAuthentication));
         Harmony.CreateAndPatchAll(typeof(UnlockAll));
+
         Logger.LogInfo($"Plugin {PluginReleaseInfo.PLUGIN_GUID} is loaded!");
-    }
-
-
-    private void Start()
-    {
-        Logger.LogInfo($"UnlockAll state: {MainProgressionContainer.UnlockAll}");
-        SceneManager.activeSceneChanged += GetSceneChange;
-    }
-
-
-    private void GetSceneChange(Scene arg0, Scene arg1)
-    {
-        //Logger.LogInfo($"Scene {arg1.name} is loaded!");
-        if(arg1.name == "ScoreScreenArcadeMode")
-        {
-            // Can use things from Helpers.RhythmManager when implemented
-        }
     }
 }
