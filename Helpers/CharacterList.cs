c4a0ffc208ceb71bbf4640cccf277bf25e9c295a
AllPoland <52187288+AllPoland@users.noreply.github.com>
2025-12-30 13:47:25 -0700

    some cleanup
    tired me makes some mistakes

--- Helpers/CharacterList.cs
+++ Helpers/CharacterList.cs
@@ -1,15 +1,36 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace UNBEATAP.Helpers;
 
 public static class CharacterList
 {
+    public static readonly string[] CharNames = [
+        "beat",
+        "beat (hoodie)",
+        "beat (guitar)",
+        "beat (up)",
+        "beat (nothing)",
+        "clef",
+        "quaver",
+        "quaver (acoustic)",
+        "quaver (cqc)",
+        "treble",
+        "rest"
+    ];
+
     private static List<string> characters = new List<string>();
 
 
     public static bool TryAddCharacter(string character)
     {
         if(characters.Contains(character))
+        {
+            Plugin.Logger.LogWarning($"Character '{character}' has already been added!");
+            return false;
+        }
+
+        if(!CharNames.Contains(character.ToLower()))
         {
             Plugin.Logger.LogWarning($"Character '{character}' does not exist!");
             return false;
