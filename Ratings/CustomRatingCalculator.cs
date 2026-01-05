c3f76709cf1ffba895820006d2cfa1649756fe96
AllPoland <52187288+AllPoland@users.noreply.github.com>
2026-01-04 19:08:51 -0700

    once again mirroring apworld changes

--- Ratings/CustomRatingCalculator.cs
+++ Ratings/CustomRatingCalculator.cs
@@ -4,8 +4,8 @@ namespace UNBEATAP.Ratings;
 
 public static class CustomRatingCalculator
 {
-    public const float DifficultyPower = 1.05f;
-    public const float ScoreFalloffBase = 0.985f;
+    public const float DifficultyPower = 1.2f;
+    public const float ScoreFalloffBase = 0.98f;
 
 
     public static float GetCustomRatingFromPlay(float level, float acc, bool fc, bool fail)
